c796d7077dc00f6061e054258bf005b3dcefb6c9
akashyt12 <neverlord198@gmail.com>
2026-09-22 07:47:51 +0530

    Fix port to 8080

--- main.py
+++ main.py
@@ -105,5 +105,5 @@ async def ocr_b64(image: str = Query(...), language: str = Query(default="en")):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
